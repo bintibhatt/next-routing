ee199582ac52d645533e3e6c9f3748ddee4a0ad2
Binti Bhatt <bintibhatt.zuru@gmail.com>
2024-01-05 11:31:31 +0530

    Dynamic routing hands-on

---
+++ src/pages/product/[productId].js
@@ -0,0 +1,9 @@
+import { useRouter } from "next/router";
+
+function ProductDetail() {
+  const router = useRouter();
+  const productId = router.query.productId;
+  return <h1>Product {productId} Detail</h1>;
+}
+
+export default ProductDetail;
