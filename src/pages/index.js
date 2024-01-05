9c9ad63d4e16b85a6fe1a27fd782bc15b38ad412
Binti Bhatt <bintibhatt.zuru@gmail.com>
2024-01-05 16:57:39 +0530

    Navigating Programmatically Hands-on

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,13 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 function Home() {
+  const router = useRouter();
+  const handleClick = () => {
+    console.log("Placing your order");
+    router.push("/product");
+  };
+
   return (
     <div>
       <h1>Home Page</h1>
@@ -11,6 +18,7 @@ function Home() {
       <Link href="/product">
         <h3>Products</h3>
       </Link>
+      <button onClick={handleClick}>Place Order</button>
     </div>
   );
 }
